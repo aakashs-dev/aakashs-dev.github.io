84121a2b6533cbb624699119f2be64f1b4702a40
Aakash <aakashx97@gmail.com>
2026-09-03 13:24:55 +0530

    feat(components): add extracurricular.ts

---
+++ src/app/components/extracurricular/extracurricular.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-extracurricular',
+  imports: [],
+  templateUrl: './extracurricular.html',
+  styleUrl: './extracurricular.scss',
+})
+export class Extracurricular {}
