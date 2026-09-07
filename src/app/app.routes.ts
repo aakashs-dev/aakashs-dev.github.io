8540610003c9aa968e213824037ac4e4c216c159
Aakash <aakashx97@gmail.com>
2026-09-07 09:30:36 +0530

    refactor(routes): remove standalone component routes to enforce single-page scroll layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,7 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
-import { Projects } from './components/projects/projects';
-import { About } from './components/about/about';
-import { Contact } from './components/contact/contact';
 
 export const routes: Routes = [
   { path: '', component: Home, pathMatch: 'full' },
-  { path: 'projects', component: Projects },
-  { path: 'about', component: About },
-  { path: 'contact', component: Contact },
   { path: '**', redirectTo: '' }
 ];
